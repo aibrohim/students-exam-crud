6ef6fd87ce077f556df014d18e3b73ac78a7f6df
aibrohim <ibrohim7840@gmail.com>
2022-03-27 15:34:48 +0500

    converted date to iso

--- js/data.js
+++ js/data.js
@@ -4,27 +4,27 @@ const students = [
     name: "Ibrohimjon",
     lastName: "Alimuhammedov",
     mark: 140.5,
-    markedDate: new Date("2021-12-05"),
+    markedDate: new Date("2021-12-05").toISOString()
   },
   {
     id: 101,
     name: "Asadbek",
     lastName: "",
     mark: 146,
-    markedDate: new Date("2021-12-06")
+    markedDate: new Date("2021-12-06").toISOString()
   },
   {
     id: 102,
     name: "Ahmadjon",
     lastName: "Hasanjanov",
     mark: 75,
-    markedDate: new Date("2021-12-01")
+    markedDate: new Date("2021-12-01").toISOString()
   },
   {
     id: 103,
     name: "G'anivoy",
     lastName: "Teshayev",
     mark: 40,
-    markedDate: new Date("2021-12-05")
+    markedDate: new Date("2021-12-05").toISOString()
   }
 ]
