ca62991625c891bbd99480069ae461066a9f5965
aibrohim <ibrohim7840@gmail.com>
2022-03-27 15:31:51 +0500

    added data which reperesents the students

---
+++ js/data.js
@@ -0,0 +1,30 @@
+const students = [
+  {
+    id: 100,
+    name: "Ibrohimjon",
+    lastName: "Alimuhammedov",
+    mark: 140.5,
+    markedDate: new Date("2021-12-05"),
+  },
+  {
+    id: 101,
+    name: "Asadbek",
+    lastName: "",
+    mark: 146,
+    markedDate: new Date("2021-12-06")
+  },
+  {
+    id: 102,
+    name: "Ahmadjon",
+    lastName: "Hasanjanov",
+    mark: 75,
+    markedDate: new Date("2021-12-01")
+  },
+  {
+    id: 103,
+    name: "G'anivoy",
+    lastName: "Teshayev",
+    mark: 40,
+    markedDate: new Date("2021-12-05")
+  }
+]
